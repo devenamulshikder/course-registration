26e8eb773843bd2eedb5c68e06a06f14e32dfd3b
devenamulshikder <devenamulshikder@gmail.com>
2024-03-15 09:40:07 +0600

    initially projects setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Courses from "./components/Courses";
+
+
+function App() {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-5xl font-bold text-center">Course Registration</h1>
+        <Courses></Courses>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Courses/index.jsx
@@ -0,0 +1,20 @@
+import Course from "./Course"
+
+
+const Courses = () => {
+  return (
+    <div>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+        <Course/>
+    </div>
+  )
+}
+
+export default Courses
